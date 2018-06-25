ff1d43bcb96784a3a3f755d5ed306661ec967b2f
Mat Ryer <thatmatryer@gmail.com>
2018-06-25 15:01:53 +0100

    removed intelligence from files in remototypes

--- remototypes/remototypes.go
+++ remototypes/remototypes.go
@@ -1,11 +1,5 @@
 package remototypes
 
-import (
-	"context"
-	"errors"
-	"io"
-)
-
 // File describes a binary file.
 // This type is only allowed in requests, for responses RPC methods should
 // return a FileResponse.
@@ -14,41 +8,44 @@ type File struct {
 	Filename  string `json:"filename"`
 }
 
-// Open opens the file as an io.ReadCloser.
-// Callers must close the file.
-func (f File) Open(ctx context.Context) (io.ReadCloser, error) {
-	opener, ok := ctx.Value(contextKeyFileOpener).(Opener)
-	if !ok {
-		return nil, errors.New("opener missing from context")
-	}
-	return opener(ctx, f)
-}
+// // Open opens the file as an io.ReadCloser.
+// // Callers must close the file.
+// func (f File) Open(ctx context.Context) (io.ReadCloser, error) {
+// 	opener, ok := ctx.Value(contextKeyFileOpener).(Opener)
+// 	if !ok {
+// 		return nil, errors.New("opener missing from context")
+// 	}
+// 	return opener(ctx, f)
+// }
 
 // FileResponse is response type for a file.
-type FileResponse struct {
-	Filename      string    `json:"filename"`
-	ContentType   string    `json:"contentType"`
-	ContentLength int       `json:"contentLength"`
-	Data          io.Reader `json:"-"`
-	Error         string    `json:"error"`
-}
+type FileResponse struct{}
 
-// Opener is a function that knows how to open files.
-type Opener func(ctx context.Context, file File) (io.ReadCloser, error)
+// FileResponse is response type for a file.
+// type FileResponse struct {
+// 	Filename      string    `json:"filename"`
+// 	ContentType   string    `json:"contentType"`
+// 	ContentLength int       `json:"contentLength"`
+// 	Data          io.Reader `json:"-"`
+// 	Error         string    `json:"error"`
+// }
 
-// WithOpener gets a new context.Context with the specified Opener.
-func WithOpener(ctx context.Context, opener Opener) context.Context {
-	return context.WithValue(ctx, contextKeyFileOpener, opener)
-}
+// // Opener is a function that knows how to open files.
+// type Opener func(ctx context.Context, file File) (io.ReadCloser, error)
 
-// contextKey is a local context key type.
-// see https://medium.com/@matryer/context-keys-in-go-5312346a868d
-type contextKey string
+// // WithOpener gets a new context.Context with the specified Opener.
+// func WithOpener(ctx context.Context, opener Opener) context.Context {
+// 	return context.WithValue(ctx, contextKeyFileOpener, opener)
+// }
 
-func (c contextKey) String() string {
-	return "remototypes context key: " + string(c)
-}
+// // contextKey is a local context key type.
+// // see https://medium.com/@matryer/context-keys-in-go-5312346a868d
+// type contextKey string
 
-// contextKeyFileOpener is the context key for a function capable of
-// opening files.
-var contextKeyFileOpener = contextKey("files")
+// func (c contextKey) String() string {
+// 	return "remototypes context key: " + string(c)
+// }
+
+// // contextKeyFileOpener is the context key for a function capable of
+// // opening files.
+// var contextKeyFileOpener = contextKey("files")
